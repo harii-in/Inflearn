e0959b0db6105f982112293ff13cf9fea76ac1e8
harii-in <charin1130@naver.com>
2022-12-02 23:30:49 +0900

    #chapter04-03

---
+++ original/chapter04_03.py
@@ -0,0 +1,61 @@
+# Chapter04-03
+# 시퀀스형 
+# 컨테이너(Container : 서로다른 자료형[list, tuple, collections.deque], Flat : 한 개의 자료형[str,bytes,bytearray,array.array, memoryview])
+# 플랫(Flat : 한 개의 자료형[str, bytes, bytearray, array.array, memoryview])
+# 가변(list, bytearray, array.array, memoryview, deque) vs 불변(tuple, str, bytes)
+# 불변(tuple, str, bytes)
+
+# 해시테이블(hashtable) -> 적은 리소스로 많은 데이터를 효율적으로 관리
+# Key에 Value를 저장하는 구조
+# 파이썬 dict 해쉬 테이블 예
+# 키 값의 연산 결과에 따라 직접 접근이 가능한 구조
+# key 값을 해싱 함수 -> 해쉬 주소 -> key에 대한 value 참조
+
+# Dict -> Key 중복 허용 X, Set -> 중복 허용 X
+# Dict 및 Set 심화
+
+# Dict 구조
+# print(__builtins__.__dict__)
+
+print()
+print()
+
+# Hash 값 확인
+t1 = (10, 20, (30, 40, 50))     # 해쉬 값은 불변형으로 이뤄져야 한다.
+t2 = (10, 20, [30, 40, 50])     
+
+print( hash(t1))
+# print(hash(t2)) # 예외
+
+print()
+print()
+
+# Dict Setdefault 예제
+source = (('k1', 'val1'),
+            ('k1', 'val2'),
+            ('k2', 'val3'),
+            ('k2', 'val4'),
+            ('k2', 'val5'))
+
+new_dict1 = {}
+new_dict2 = {}
+
+# No use setdefault
+for k, v in source:
+    if k in new_dict1:
+        new_dict1[k].append(v)
+    else:
+        new_dict1[k] = [v]
+
+print(new_dict1)
+
+# Use setdefault
+for k, v in source:
+    new_dict2.setdefault(k, []).append(v)
+
+print(new_dict2)
+
+# 주의
+new_dict3 = {k : v for k , v in source}
+
+print(new_dict3)
